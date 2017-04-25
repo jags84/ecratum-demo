f66bbbc8efcdad8445304d9924fc9fe5bc9a6246
Juan Gonzalez <jags84@gmail.com>
2017-04-25 08:30:42 -0400

    Change DB Name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_EcratumDemo_session'
+Rails.application.config.session_store :cookie_store, key: '_ecratum_demo_session'
